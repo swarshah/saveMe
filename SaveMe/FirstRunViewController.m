0a740435662e41b84544e257ef2ff30571b302f9
SwarShah <swar12894@gmail.com>
2015-04-08 13:35:18 -0400

    Assigned new view controller to it’s class

---
+++ SaveMe/FirstRunViewController.m
@@ -0,0 +1,38 @@
+//
+//  FirstRunViewController.m
+//  SaveMe
+//
+//  Created by Swar on 4/8/2015.
+//  Copyright (c) 2015 Swar. All rights reserved.
+//
+
+#import "FirstRunViewController.h"
+
+@interface FirstRunViewController ()
+
+@end
+
+@implementation FirstRunViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
