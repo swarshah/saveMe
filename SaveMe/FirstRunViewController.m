8860ef57c1bda5bd6a950a21b0af0f839fae4558
SwarShah <swar12894@gmail.com>
2015-04-09 19:59:32 -0400

    Segue and hiding nag bar

--- SaveMe/FirstRunViewController.m
+++ SaveMe/FirstRunViewController.m
@@ -28,6 +28,11 @@
     return self;
 }
 
+//Hiding navigation bar
+- (void) viewWillAppear:(BOOL)animated{
+    [[self navigationController] setNavigationBarHidden:YES];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -46,7 +51,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+//Saves all values
 - (IBAction)saveButton:(id)sender {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:@"false" forKey: @"firstRun"];
@@ -56,8 +61,7 @@
     [defaults setObject:self.cEmail.text forKey: @"cEmail"];
     [defaults synchronize];
     NSLog(@"Saved all data");
-    UIStoryboard *main = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    UIViewController *vc = [main instantiateViewControllerWithIdentifier:@"firstScreen"];
-    [self presentViewController:vc animated:YES completion:nil ];
+    //Segue push
+    [self performSegueWithIdentifier:@"AssociateBack" sender:NULL];
 }
 @end
